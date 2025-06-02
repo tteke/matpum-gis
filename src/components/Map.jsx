ea2787ebc7cc91107a50cc5a16ab721f067af05a
Turker Teke <trkrtk@gmail.com>
2025-06-02 12:22:42 +0300

    fix map height

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -11,16 +11,17 @@ export default function Map() {
   const position = [51.505, -0.09];
 
   return (
-    <MapContainer center={position} zoom={13} scrollWheelZoom={false} style={{ height: '1000px' }}>
-      <TileLayer
+    <div className="h-screen">
+      <MapContainer className="h-full" center={position} zoom={13} scrollWheelZoom={false}> <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={position}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-    </MapContainer>
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
   )
 }
